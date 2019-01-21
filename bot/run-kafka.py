be9391a57420a08be65dc4a5de2f7567d37e4158
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-01-21 10:43:25 -0200

    Add kafka services config
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- bot/run-kafka.py
+++ bot/run-kafka.py
@@ -8,6 +8,8 @@ from rasa_core.interpreter import NaturalLanguageInterpreter
 from rasa_core.tracker_store import TrackerStore
 from rasa_core.channels.console import CmdlineInput
 from rasa_core import constants
+from rasa_core.broker import KafkaProducer
+from rasa_core.tracker_store import InMemoryTrackerStore
 
 logger = logging.getLogger(__name__)
 configure_colored_logging(loglevel='DEBUG')
@@ -16,12 +18,19 @@ def run(core_dir, nlu_dir):
     _endpoints = AvailableEndpoints.read_endpoints(None)
     _interpreter = NaturalLanguageInterpreter.create(nlu_dir)
 
+    kafka_broker = KafkaProducer(host='localhost:29092',
+                                 topic='rasa_core_events')
+
+    _tracker_store = InMemoryTrackerStore(domain='localhost:29092',event_broker=kafka_broker)
+
     _agent = load_agent(core_dir,
                         interpreter=_interpreter,
+                        tracker_store=_tracker_store,
                         endpoints=_endpoints)
 
     http_server = start_server([CmdlineInput()], "", "", 5005, _agent)
     start_cmdline_io(constants.DEFAULT_SERVER_FORMAT.format(5005), http_server.stop)
+
 
     try:
         http_server.serve_forever()

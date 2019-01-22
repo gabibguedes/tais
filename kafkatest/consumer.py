ce3b37cc7676b12cc1dae3a8b361bace8dda3937
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-01-22 09:49:04 -0200

    Add consumer and producer using authentication
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- kafkatest/consumer.py
+++ kafkatest/consumer.py
@@ -3,9 +3,13 @@ from kafka import KafkaConsumer
 from json import loads
 
 consumer = KafkaConsumer(
-    'rasa_core_events',
-     bootstrap_servers=['localhost:29092'],
-     value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    'kafka-python-topic',
+     bootstrap_servers=['localhost:29095'],
+     value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+     sasl_plain_username='admin',
+     sasl_plain_password='admin-secret',
+     sasl_mechanism='PLAIN',
+     security_protocol='SASL_PLAINTEXT')
 
 for message in consumer:
     print("ENTROU aki")
--- kafkatest/producer.py
+++ kafkatest/producer.py
@@ -5,8 +5,12 @@ from time import sleep
 from datetime import datetime
 
 # Create an instance of the Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:29092',
-                            value_serializer=lambda v: str(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='localhost:29093',
+                         value_serializer=lambda v: str(v).encode('utf-8'),
+                         sasl_plain_username='admin',
+                         sasl_plain_password='admin-secret',
+                         sasl_mechanism='PLAIN',
+                         security_protocol='SASL_PLAINTEXT')
 
 # Call the producer.send method with a producer-record
 print("Ctrl+c to Stop")
